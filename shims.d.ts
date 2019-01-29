dfdca77e71679c995fb8bb47d77623562d6c424e
huangchinlin <huangchin.lin@gmail.com>
2019-01-29 16:44:35 +0800

    更新。

--- shims.d.ts
+++ shims.d.ts
@@ -1,20 +1,25 @@
 // Auto-generated. Do not edit.
 
-//% color=50 weight=19
-//% icon="\uf1eb" block="Mbit_IR"
+
+
+    //% color=50 weight=19
+    //% icon="\uf1eb" block="Mbit_IR"
 declare namespace Mbit_IR {
+
     /**
      * button pushed.
      */
     //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
+    //% block="on |%btn| button pressed"
+    //% parts="Mbit_IR" shim=Mbit_IR::onPressEvent
     function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=Mbit_IR::init
+    //% block="connect ir receiver to %pin"
+    //% parts="Mbit_IR" shim=Mbit_IR::init
     function init(pin: Pins): void;
 }
 
